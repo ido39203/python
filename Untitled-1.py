f6eb39dde9c47ef0f288d4c7af66421b7a34daee
ido39203 <79574128+ido39203@users.noreply.github.com>
2021-12-23 02:10:10 +0900

    예제1
    안녕하세요

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+# 1. 정수
+num1 = -99
+num2 = 99
+print(f"abs({num1}) = {abs(num1)}") # abs(num1)
+print(f'abs({num2}) = {abs(num2)}') # abs(num2)
+ 
+# 2. 실수
+num3 = -99.99
+num4 = 99.99
+print(f'abs({num3}) = {abs(num3)}') # abs(num3)
+print(f'abs({num4}) = {abs(num4)}') # abs(num4)
+ 
+# 3. 0의 절대값
+num5 = 0
+num6 = 0.0
+print(f'abs({num5}) = {abs(num5)}') # abs(num5)
+print(f'abs({num6}) = {abs(num6)}') # abs(num6)
---
+++ result.py
@@ -0,0 +1,8 @@
+result=0
+for a in range(1,101):
+    result=result+a
+
+    if result > 100:
+        print(a)
+        break
+print(result)        
